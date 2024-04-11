98efb43a832d86156698127cdc2c6904017ffb87
Zara Griffen <zaragriffen@Zaras-MacBook-Air.local>
2024-04-11 20:49:58 +0100

    print name -

--- main.py
+++ main.py
@@ -2,9 +2,12 @@
 
 def print_hello(name):
     """Print a greeting to the user """
-    pass
+
+    print(f"Hi {name}") 
+    
 
 
 if __name__ == "__main__":
     name = input("What's your name?\n")
     print_hello(name)
+    
